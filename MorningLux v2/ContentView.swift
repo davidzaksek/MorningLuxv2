c446ad505858b1d213ab07f2ed43787868e3331d
Klemen Selakovic <klemen.selakovic@gmail.com>
2023-03-31 09:37:10 +0200

    Initial Commit

---
+++ MorningLux v2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MorningLux v2
+//
+//  Created by Klemen Selakovic on 31/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
